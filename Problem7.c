05d855914002189e35d0ef42d834f531ec43efec
kthxxx <152075319+kthxxx@users.noreply.github.com>
2025-02-25 18:30:04 +0800

    Create Problem7.c

---
+++ Problem7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    float remaining = 12, original;
+    original = remaining * 5 / 1;  
+    printf("Original amount: %.2f\n", original);
+    
+    return 0;
+}
